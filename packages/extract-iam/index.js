78af95c0ac533f04d917ede5e1c33c5de1f2fbec
dsandor <david.sandor@gmail.com>
2020-01-28 08:44:14 -0500

    fix index.js to fix bin

--- packages/extract-iam/index.js
+++ packages/extract-iam/index.js
@@ -1,28 +1,30 @@
-import Extract from './Extract';
-import { option, parse, roleName, J, Y } from 'commander';
+#!/usr/bin/env node
+const Extract = require('./Extract'),
+	program = require('commander');
 
-option('-r --role-name <type>', 'IAM Role name to extract.')
+program
+	.option('-r --role-name <type>', 'IAM Role name to extract.')
 	.option('-y', 'Output in YAML format.')
 	.option('-j', 'Output in JSON format.');
 
-parse(process.argv);
+program.parse(process.argv);
 
 async function run() {
-	const extractor = new Extract(roleName);
+	const extractor = new Extract(program.roleName);
 	let resource;
 
 	try {
-		resource = await extractor.extract(roleName);
+		resource = await extractor.extract(program.roleName);
 	} catch (e) {
 		console.error('Failed extracting role:', e.message);
 		process.exit(1);
 	}
 
-	if (J) {
+	if (program.J) {
 		console.log('\n', JSON.stringify(resource, null, 2));
 	}
 
-	if (Y || (!Y && !J)) {
+	if (program.Y || (!program.Y && !program.J)) {
 		console.log('\n', extractor.toYaml(resource));
 	}
 }
